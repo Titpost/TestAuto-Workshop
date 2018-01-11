a341a36aca39fab3d574de3ce4c0883f39782d5b
Sergei Titov <Sergei_Titov@epam.com>
2018-01-11 22:37:19 +0300

    Common base-class was segregated

--- src/test/java/day01/SeleniumTest.java
+++ src/test/java/day01/SeleniumTest.java
@@ -4,116 +4,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.function.Function;
 
-//6. Create a new test in a new Java class, specify test name in accordance with checking functionality
-public class SeleniumTest {
+/**
+ * Common utility base class for Selenium tests.
+ */
+class SeleniumTest {
 
-    private WebDriver driver;
+    WebDriver driver;
 
-    private HashMap<String, String> texts = new HashMap<>();
-
-
-    @BeforeTest
-    void init() {
-        //1. Browser - Chrome
-        driver = new ChromeDriver();
-        //2. Window - maximized
-        driver.manage().window().maximize();
-
-        texts.put("Login", "epam");
-        texts.put("Password", "1234");
-        texts.put("span", "PITER CHAILOVSKII");
-        texts.put("main-title", "EPAM FRAMEWORK WISHES…");
-
-        texts.put("main-txt", "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT, SED DO EIUSMOD TEMPOR " +
-                "INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION " +
-                "ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO CONSEQUAT DUIS AUTE IRURE DOLOR IN REPREHENDERIT " +
-                "IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.");
-
-        texts.put("benefit-txt0", "To include good practices\n" +
-                "and ideas from successful\n" +
-                "EPAM projec");
-
-        texts.put("benefit-txt1", "To be flexible and\n" +
-                "customizable");
-
-        texts.put("benefit-txt2", "To be multiplatform");
-
-        texts.put("benefit-txt3", "Already have good base\n" +
-                "(about 20 internal and\n" +
-                "some external projects),\n" +
-                "wish to get more…");
-    }
-
-    //3. All code should be formatted by functional blocks separated by functionality and meaning
-    @Test
-    public void testFrameworkWithChrome() {
-
-        //7. Open test site by URL
-        driver.navigate().to("https://jdi-framework.github.io/tests");
-
-        //8. Assert Browser title
-        Assert.assertEquals(driver.getTitle(), "Index Page");
-
-        //9. Perform login
-        final WebElement elementToggle = driver.findElement(By.className("uui-profile-menu"));
-        elementToggle.click();
-        fillElement("Login");
-        fillElement("Password");
-        final WebElement elementSubmit = driver.findElement(By.className("btn-login"));
-        elementSubmit.click();
-
-        //10. Assert User name in the left-top side of screen that user is logged in
-        final WebElement elementPhoto = driver.findElement(By.className("profile-photo"));
-        assertElementText(elementPhoto,"span", By::tagName);
-
-        //11. Assert Browser title
-        Assert.assertEquals(driver.getTitle(), "Index Page");
-
-        //12. Assert that there are 4 images on the Home Page and they are displayed
-        int benefitsCount = 4;
-        final List<WebElement> benefitImages = driver.findElements(By.className("benefit-icon"));
-        Assert.assertEquals(benefitImages.size(), benefitsCount);
-        for (WebElement img : benefitImages) {
-            Assert.assertTrue(img.isDisplayed());
-        }
-
-        //13. Assert that there are 4 texts on the Home Page and check them by getting texts
-        final List<WebElement> benefitTexts = driver.findElements(By.className("benefit-txt"));
-        Assert.assertEquals(benefitTexts.size(), benefitsCount);
-        for (byte i = 0; i < benefitsCount; i++) {
-            Assert.assertTrue( benefitTexts.get(i).isDisplayed());
-            Assert.assertEquals(
-                    benefitTexts.get(i).getText(),
-                    texts.get("benefit-txt" + i)
-            );
-        }
-
-        //14. Assert that there are the main header and the text below it on the Home Page
-        assertElementText(driver,"main-title", By::className);
-        assertElementText(driver,"main-txt", By::className);
-    }
-
-    @AfterTest
-    void close() {
-        //15. Close Browser
-        driver.close();
-    }
+    HashMap<String, String> texts = new HashMap<>();
 
     /**
      * Fills the element with value from map by its id.
      * @param key - id of Element
      */
-    private void fillElement(String key) {
+    void fillElement(String key) {
         final WebElement element = driver.findElement(By.id(key));
         element.sendKeys(texts.get(key));
     }
@@ -123,9 +32,8 @@ public class SeleniumTest {
      * @param by - attribute to find by
      * @param finder - corresponding "By" method
      */
-    private void assertElementText(SearchContext context,  String by, Function<String, By> finder) {
+    void assertElementText(SearchContext context, String by, Function<String, By> finder) {
         final WebElement element = context.findElement(finder.apply(by));
         Assert.assertEquals(texts.get(by), element.getText());
     }
 }
-
